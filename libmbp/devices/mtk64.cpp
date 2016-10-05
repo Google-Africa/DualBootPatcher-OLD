3ebe908c0612e0ad900a0fa6c7e0a0be3096f4d5
Coulibaly Nonta <coulibalynonta@gmail.com>
2016-10-05 14:15:36 +0100

    ZTE Blade V6 has been added

--- libmbp/devices/mtk64.cpp
+++ libmbp/devices/mtk64.cpp
@@ -159,6 +159,39 @@ void addMtk64Devices(std::vector<Device *> *devices)
     device->twOptions()->defaultBrightness = 162;
     device->twOptions()->cpuTempPath = "/sys/class/thermal/thermal_zone1/temp";
     devices->push_back(device);
+ 
+    // ZTE Blade V6
+    device = new Device();
+    device->setId("Blade_V6");
+    device->setArchitecture(ARCH_ARM64_V8A);
+    device->setCodenames({ "Blade_V6", "aeon6735_65u_nj_l", "P635A20"});
+    device->setName("ZTE Blade V6");
+    device->setBlockDevBaseDirs({ MTK_BASE_DIR, MTK_11230000_BASE_DIR });
+    device->setSystemBlockDevs({ MTK_SYSTEM, MTK_11230000_SYSTEM,
+        "/dev/block/mmcblk0p20" });
+    device->setCacheBlockDevs({ MTK_CACHE, MTK_11230000_CACHE,
+        "/dev/block/mmcblk0p21" });
+    device->setDataBlockDevs({ MTK_USERDATA, MTK_11230000_USERDATA,
+        "/dev/block/mmcblk0p22" });
+    device->setBootBlockDevs({ MTK_BOOT, MTK_11230000_BOOT,
+        "/dev/block/mmcblk0p7" });
+    device->setRecoveryBlockDevs({ MTK_RECOVERY, MTK_11230000_RECOVERY,
+        "/dev/block/mmcblk0p8" });
+    device->setExtraBlockDevs({ MTK_METADATA, MTK_11230000_METADATA,
+        MTK_NVRAM, MTK_11230000_NVRAM,
+        MTK_LK, MTK_11230000_LK,
+        MTK_LOGO, MTK_11230000_LOGO,
+        MTK_PARA, MTK_11230000_PARA,
+        MTK_TEE1, MTK_11230000_TEE1,
+        MTK_TEE2, MTK_11230000_TEE2 });
+    device->twOptions()->supported = true;
+    device->twOptions()->graphicsBackends = { "fbdev" };
+    device->twOptions()->flags = Device::FLAG_TW_GRAPHICS_FORCE_USE_LINELENGTH;
+    device->twOptions()->pixelFormat = Device::TwPixelFormat::RGBX_8888;
+    device->twOptions()->maxBrightness = 255;
+    device->twOptions()->defaultBrightness = 162;
+    device->twOptions()->cpuTempPath = "/sys/class/thermal/thermal_zone1/temp";
+    devices->push_back(device);
 }
 
 }
